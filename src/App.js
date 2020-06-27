c7fa7341f6c9477358dea3488d12c5b237a4a659
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-06-27 15:03:00 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [quotes, setQuotes] = useState ([])
+
+  useEffect (()=> {
+    const fetchData = async () => {
+    const result = await fetch('https://cyf-tayfuntayfun.herokuapp.com/quotes/random')
+      .then(response => response.json())
+      setQuotes(result);
+      console.log(quotes)
+    }
+  fetchData();
+  }, []);
+
+    const handleClick =() => {
+      window.location.reload(true)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    quotes ? 
+    <div className="quote-box">
+      <div className='quote-box-quote'>
+        <div>
+          <p style={{
+                    fontSize:'24px'
+                    }}>{quotes.quote}</p>
+        </div>
+        <div>
+          <p style={{
+                    color:'yellow'
+                    }}>{quotes.author}</p>
+        </div>
+      </div>
+      <div className='button-box'>
+        <button 
+            style={{backgroundColor:'#263238',
+                    border: '1px solid gray',
+                    borderRadius:'3px',
+                    padding:'10px',
+                    color:'white'
+                    }}
+            onClick={handleClick}
+            ><b>New Quote</b>
+        </button>
+      </div>
+    </div> : null
   );
 }
 
